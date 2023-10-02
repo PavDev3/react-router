96d67c88092eb80734654da57303132d85316d85
Pablo <pnunfe@gmail.com>
2023-10-02 12:58:53 +0200

    update searchpage

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect } from 'react'
+import { Link } from '../Link'
 
 export default function SearchPage ({ routeParams }) {
   useEffect(() => {
@@ -7,6 +8,11 @@ export default function SearchPage ({ routeParams }) {
   }, [routeParams.query])
 
   return (
-    <h1>Has buscado {routeParams.query}</h1>
+    <>
+      <h1>Has buscado {routeParams.query} y te devolvemos lo que encontramos aquí.</h1>
+      <p>Si no encuentras lo que buscas, puedes volver a la página principal y buscar otra cosa.</p>
+      <p>Gracias por tu preferencia.</p>
+    <Link to="/">Home</Link>
+    </>
   )
 }
